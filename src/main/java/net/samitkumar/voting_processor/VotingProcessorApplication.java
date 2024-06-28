8a9092ffb8ae81233176f804ff68d4cfe20f86ba
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-06-28 16:54:17 +0200

    first commit

---
+++ src/main/java/net/samitkumar/voting_processor/VotingProcessorApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.voting_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/voting_processor/VotingProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.voting_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
